03f352fcbfe029e08841687b67694f3b1432aaa0
gau <gau@MacBook-Pro.local>
2016-03-07 12:27:35 +0900

    0.5.1 first commit

---
+++ README.md
@@ -0,0 +1,112 @@
+# README
+
+現在の線幅に対する割合（パーセント）を指定すると、オブジェクトが持っている線すべての幅を一気に変更します。
+
+<div class="fig center" style="margin-bottom: 20px;"><img src="http://www.graphicartsunit.com/saucer/images/resize-all-stroke-width/eye.png" alt="イメージ" class="noshadow"></div>
+
+
+### 更新履歴
+
+* 0.5.1：新規作成（公開）
+
+----
+
+### 対応バージョン
+
+* Illustrator CS5／CS6／CC／CC 2014／CC 2015
+
+----
+
+### ダウンロード
+
+* [スクリプトをダウンロードする](https://github.com/gau/resize-all-stroke-width/archive/master.zip)
+
+----
+
+### インストール方法
+
+1. ダウンロードしたファイルを解凍します。
+2. 所定の場所に「すべての線幅を変更.jsx」をコピーします。Windows版ではお使いのIllustratorの種類によって保存する場所が異なりますのでご注意ください。
+3. Illustratorを再起動します。
+4. `ファイル > スクリプト > すべての線幅を変更`と表示されていればインストール成功です。
+
+#### ファイルをコピーする場所
+
+| OS | バージョン | フォルダの場所 |
+|:-----|:-----|:-----|
+| Mac | 全 | /Applications/Adobe Illustrator *(ver)*/Presets/ja_JP/スクリプト/ |
+| 32bit Win | CS5まで | C:\Program Files\Adobe\Adobe Illustrator *(ver)*\Presets\ja_JP\スクリプト\ |
+| 64bit Win | CS5, CS6（32bit版） | C:\Program Files (x86)\Adobe\Adobe Illustrator *(ver)*\Presets\ja_JP\スクリプト\ |
+| 64bit Win | CS6（64bit版）以降 | C:\Program Files\Adobe\Adobe Illustrator *(ver)* (64 Bit)\Presets\ja_JP\スクリプト\ |
+
+* *(ver)*にはお使いのIllustratorのバージョンが入ります
+* 本スクリプトは、CS4以前では動作を検証しておりません
+
+----
+
+### 通常操作で線幅を変更する
+
+<div class="fig center"><img src="http://www.graphicartsunit.com/saucer/images/resize-all-stroke-width/fig01.png" alt="通常操作で線幅を変更する" class="noshadow"></div>
+
+上図左端のように、2つの線が追加されたオブジェクトがあるとします。［線パネル］を使って、このオブジェクトの線幅を調整すると、どちらか一方だけが変更されることになります。変更されなかったほうの線幅を変えるには、［アピアランスパネル］で一度クリックして選択してから作業する必要があります。例えば、これが5本の線を持つオブジェクトなら、同じ操作を5回繰り返すことになるわけです。
+
+----
+
+### このスクリプトを使うと
+
+<div class="fig center"><img src="http://www.graphicartsunit.com/saucer/images/resize-all-stroke-width/fig02.png" alt="このスクリプトを使うと" class="noshadow"></div>
+
+オブジェクトを選択した状態でスクリプトを実行すると、比率を入力するダイアログが表示されます。現在の線幅に対し、変更したい比率（パーセント単位）を半角数字で入力して実行すると、オブジェクトが持つすべての線の幅を一気に変更できます。複数のオブジェクトを選択した状態でも実行可能です。
+
+----
+
+### テキストの基本属性は対象外
+
+<div class="fig center"><img src="http://www.graphicartsunit.com/saucer/images/resize-all-stroke-width/fig03.png" alt="テキストの基本属性は対象外" class="noshadow"></div>
+
+テキストオブジェクトの線には2つの種類があります。長くなるのでここでは詳細に触れませんが、ざっくり言うと［アピアランスパネル］を使って追加した線と、もともとテキストオブジェクトが持っている基本属性としての線です。基本属性の線は、スクリプト側から操作するのが難しいため、現時点では対象外としています。［アピアランスパネル］で追加した線だけが対象です。いい方法を思いついたら、後日追加してバージョンアップするかもしれません（しないかもしれません）。
+
+----
+
+### プレビューをカスタマイズする
+
+このスクリプトでは、ダイアログで数値を1文字入力するごとにリアルタイムで結果がプレビューされます。しかし、処理に時間がかかるものなどは、待ち時間が発生して負担に感じるかもしれません。プレビューの方法は2種類用意していますので、希望する動作に合わせてカスタマイズしてご利用ください。
+
+スクリプトの14行目にある`onChanging`の値（boolean型）を変更します。内容は以下の通りです。
+
+|値|内容|
+|:-----|:-----|
+|true|値を変更するたびにプレビュー更新（初期値）|
+|false|比率のテキストフィールドからフォーカスがはずれたとき、または、CS6以降では、command（Windows）、control（Ctrl）、option（Alt）のいずれかのキーを押したタイミングでプレビュー更新|
+
+* 値を変更するたびに更新する場合 → `'onChanging' : true`  
+* 値を変更するたびに更新しない場合 → `'onChanging' : false`
+
+----
+
+### 注意
+
+* **データが多い時は処理に時間がかかります。必ず事前にドキュメントを保存してから実行してください。**
+* 必要なオブジェクトが選択されていないときは、警告を表示して処理を中断します。
+* テキスト基本属性の線や、スタティックシンボル内の線などは処理できません。
+* ダイナミックシンボルは、インスタンス内のオブジェクトが選択されているときだけ処理します。インスタンス全体が選択されているときは処理できません。
+* グループ内のオブジェクトも処理されます。
+* オブジェクトの種類や構造によって意図しない結果になる可能性もゼロではありません。
+
+----
+
+### 免責事項
+
+* このスクリプトを使って起こったいかなる現象についても制作者は責任を負えません。すべて自己責任にてお使いください。
+* CS5からCC 2015で動作の確認はしましたが、OSのバージョンやその他の状況によって実行できないことがあるかもしれません。もし動かなかったらごめんなさい。
+
+----
+
+### ライセンス
+
+* すべての線幅を変更.jsx
+* Copyright (c) 2016 Toshiyuki Takahashi
+* Released under the MIT license
+* [http://opensource.org/licenses/mit-license.php](http://opensource.org/licenses/mit-license.php)
+* Created by Toshiyuki Takahashi ([Graphic Arts Unit](http://www.graphicartsunit.com/))
+* [Twitter](https://twitter.com/gautt)
---
+++ すべての線幅を変更.jsx
@@ -0,0 +1,223 @@
+/*
+すべての線幅を変更.jsx
+Copyright (c) 2016 Toshiyuki Takahashi
+Released under the MIT license
+http://opensource.org/licenses/mit-license.php
+http://www.graphicartsunit.com/
+*/
+(function() {
+
+	// Settings
+	var settings = {
+		'scale' : 50,
+		'preview' : true,
+		'onChanging' : true
+	}
+
+	// Initialize
+	var SCRIPT_TITLE = 'すべての線幅を変更';
+	var SCRIPT_VERSION = '0.5.1';
+
+	var doc = app.activeDocument;
+	var targetItems = doc.selection;
+	if(typeof settings.preview != 'boolean') settings.preview = false;
+	if(typeof settings.onChanging != 'boolean') settings.onChanging = false;
+
+	// Load setting from json file
+	var saveOptions = {
+		'os' : File.fs,
+		'jsxPath' : $.fileName,
+		'reverseDomain' : 'com.graphicartsunit',
+		'fileName' : 'resize_all_stroke_width.json',
+		'path' : ''
+	};
+	saveOptions.path = getSettingFilePath(saveOptions);
+	loadSettings();
+
+	// UI dialog
+	function MainDialog() {
+		this.init();
+		return this;
+	};
+	MainDialog.prototype.init = function() {
+		var unit = 10;
+		var thisObj = this;
+		thisObj.dlg = new Window('dialog', SCRIPT_TITLE + ' - ver.' + SCRIPT_VERSION);
+		thisObj.dlg.margins = [unit, unit * 3, unit, unit * 3];
+
+		// Dialog - Text field
+		thisObj.scaleGroup = thisObj.dlg.add('group', undefined);
+		thisObj.scaleGroup.margins = [unit, unit, unit, unit];
+
+		thisObj.scaleGroup.add('statictext', undefined, '比率', {alignment:'left'});
+		thisObj.scaleText = thisObj.scaleGroup.add('edittext', undefined, settings.scale);
+		thisObj.scaleText.minimumSize = [unit * 6, unit];
+		thisObj.scaleGroup.add('statictext', undefined, '%', {alignment:'left'});
+
+		// Dialog - Hint message
+		var aiv = app.version.split('.')[0];
+		if(!settings.onChanging && Number(aiv) > 15) {
+			thisObj.noteGroup = thisObj.dlg.add('panel', undefined, 'HINT:');
+
+			var noteText = saveOptions.os == 'Windows' ? '［Alt］、［Ctrl］' : '［command］［option］［control］';
+			thisObj.noteText = thisObj.noteGroup.add('statictext', undefined, 'CS6以降は' + noteText + 'のいずれかを押してもプレビューを更新できます', {multiline:true});
+			thisObj.noteText.alignment = 'center';
+			thisObj.noteText.minimumSize = [unit * 25, undefined];
+		}
+
+		// Dialog - Footer group
+		thisObj.footerGroup = thisObj.dlg.add('group', undefined);
+		thisObj.footerGroup.margins = [unit, unit/2, unit, unit * 0];
+		thisObj.footerGroup.alignment = 'center';
+		thisObj.footerGroup.orientation = 'row';
+
+		// Dialog - Preview checkbox
+		thisObj.checkboxGroup = thisObj.footerGroup.add('group', undefined);
+		thisObj.checkboxGroup.margins = [0, 0, unit, 0];
+
+		thisObj.previewCheckbox = thisObj.checkboxGroup.add('checkbox', undefined, 'プレビュー');
+		thisObj.previewCheckbox.value = settings.preview;
+
+		// Dialog - Buttons
+		thisObj.buttonGroup = thisObj.footerGroup.add('group', undefined);
+
+		thisObj.cancel = thisObj.buttonGroup.add('button', undefined, 'キャンセル', {name: 'cancel'});
+		thisObj.ok = thisObj.buttonGroup.add('button', undefined, '実行', { name:'ok'});
+
+		// Preview
+		function preview(event) {
+			var previewScale = validateScale(settings.preview ? thisObj.scaleText.text : 100);
+			if(previewScale != -1) {
+				settings.scale = settings.preview ? previewScale : thisObj.scaleText.text;
+				resizeStrokeWidth(previewScale);
+				var dummyObject = doc.pathItems.add();
+				dummyObject.remove();
+				app.redraw();
+				app.undo();
+				thisObj.scaleText.active = true;
+			}
+			thisObj.scaleText.text = settings.scale;
+		}
+
+		// Set event for preview
+		thisObj.scaleText.active = true;
+		var previewEvent = settings.onChanging ? 'changing' : 'change';
+		thisObj.scaleText.addEventListener(previewEvent, preview);
+		thisObj.scaleText.dispatchEvent(new UIEvent(previewEvent));
+		if(!settings.onChanging) {
+			thisObj.scaleText.addEventListener('keyup', function(e) {
+				if(e.keyName == 'Alt' || e.keyName == 'Meta' || e.keyName == 'Control') {
+					thisObj.scaleText.dispatchEvent(new UIEvent(previewEvent));
+				}
+			});
+		}
+
+		// Preview checkbox action
+		thisObj.previewCheckbox.onClick = function() {
+			settings.preview = this.value;
+			thisObj.scaleText.dispatchEvent(new UIEvent(previewEvent));
+		}
+
+		// Button action
+		thisObj.ok.onClick = function() {
+			try {
+				var scale = validateScale(thisObj.scaleText.text);
+				if(scale != -1) {
+					settings.scale = scale;
+					resizeStrokeWidth(scale);
+					saveSettings();
+					thisObj.closeDialog();
+				} else {
+					thisObj.scaleText.text = settings.scale;
+				}
+			} catch(e) {
+				alert('エラーが発生して処理を実行できませんでした\nエラー内容：' + e);
+			}
+		}
+		thisObj.cancel.onClick = function() {
+			thisObj.closeDialog();
+		}
+	};
+	MainDialog.prototype.showDialog = function() {
+		this.dlg.show();
+	};
+	MainDialog.prototype.closeDialog = function() {
+		this.dlg.close();
+	};
+
+	var dialog = new MainDialog();
+
+	if (!doc) {
+		alert('対象ドキュメントがありません');
+	} else if (targetItems.length <= 0) {
+		alert('少なくとも1つ以上のオブジェクトを選択してください');
+	} else {
+		dialog.showDialog();
+	}
+
+	// Main Process
+	function resizeStrokeWidth(scale) {
+		for (var i = 0; i < targetItems.length; i++) {
+			targetItems[i].resize(100, 100, true, true, true, true, scale, Transformation.CENTER);
+		}
+	}
+
+	// Validation for scale value
+	function validateScale(v) {
+		if(!isNaN(v)) {
+			if(v <= 0) {
+				alert('0以下の値は1％として処理されます');
+				return 1;
+			} else if(v > 1000) {
+				alert('最大値は1000より大きい値は1000％として処理されます');
+				return 1000;
+			}
+			return v;
+		} else {
+			alert('入力できるのは半角数字のみです');
+			return -1;
+		}
+	}
+
+	// Get path of json file
+	function getSettingFilePath(options) {
+		var filepath = '';
+		switch(options.os) {
+			case 'Macintosh':
+				filepath = Folder.userData + '/' + options.reverseDomain + '/Illustrator/Scripts/' + options.fileName;
+				break;
+			case 'Windows':
+				filepath = Folder.userData + '/' + options.reverseDomain + '/Illustrator/Scripts' + options.fileName;
+				break;
+			default :
+				break;
+		}
+		return filepath;
+	}
+
+	// Load settings from json file
+	function loadSettings() {
+		var dir = saveOptions.path.match(/(.*)(\/)/)[1];
+		if(!new Folder(dir).exists) {
+			new Folder(dir).create();
+		} else if(new File(saveOptions.path).exists) {
+			var settingFile = new File(saveOptions.path);
+			settingFile.encoding = 'UTF-8';
+			settingFile.open('r');
+			var loadedSettings = settingFile.readln();
+			loadedSettings = (new Function('return' + loadedSettings))();
+			settingFile.close();
+			loadedSettings.onChanging = settings.onChanging;
+			settings = loadedSettings;
+		}
+	}
+
+	// Save settings to json file
+	function saveSettings() {
+		var settingFile = new File(saveOptions.path);
+		settingFile.open('w');
+		settingFile.write(settings.toSource());
+		settingFile.close();
+	}
+
+}());
